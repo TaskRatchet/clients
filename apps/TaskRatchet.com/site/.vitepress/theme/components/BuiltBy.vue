b5336e71c7305ae34429125e9d11b66867527fbf
Nathan Arthur <nathan@nathanarthur.com>
2024-02-12 12:13:19 -0500

    fix team title spacing

--- apps/TaskRatchet.com/site/.vitepress/theme/components/BuiltBy.vue
+++ apps/TaskRatchet.com/site/.vitepress/theme/components/BuiltBy.vue
@@ -6,65 +6,71 @@ defineProps<{
 
 <template>
     <article class="BuiltBy">
-        <h1 class="main-title">Team</h1>
         <div class="container">
-            <div class="bio">
-                <div class="img">
-                    <img src="/Nathan-Arthur.jpg" alt="Nathan Arthur" />
+            <h1 class="main-title">Team</h1>
+            <div class="bios">
+                <div class="bio">
+                    <div class="img">
+                        <img src="/Nathan-Arthur.jpg" alt="Nathan Arthur" />
+                    </div>
+                    <div class="content">
+                        <h2 class="title">Founded by Narthur</h2>
+                        <p class="details">
+                            TaskRatchet was built by <a href="https://nathanarthur.com">Nathan Arthur</a>, a web developer
+                            active in <a href="https://forum.beeminder.com/u/narthur/summary">the Beeminder community</a>.
+                            Nathan has been <a href="https://www.beeminder.com/narthur">using Beeminder since 2018</a>. With
+                            the
+                            encouragement of Beeminder cofounder Danny and the Beeminder community, Nathan launched
+                            TaskRatchet <a href="https://blog.beeminder.com/taskratchet/">in 2020</a> to make it easier to
+                            make
+                            monetary commitments on one-off tasks. Since then he's been making <a
+                                href="https://forum.beeminder.com/t/taskratchet-development-updates/5037">steady
+                                progress</a> on
+                            turning TaskRatchet into the
+                            ultimate todo list with consequences.
+                        </p>
+                    </div>
                 </div>
-                <div class="content">
-                    <h2 class="title">Founded by Narthur</h2>
-                    <p class="details">
-                        TaskRatchet was built by <a href="https://nathanarthur.com">Nathan Arthur</a>, a web developer
-                        active in <a href="https://forum.beeminder.com/u/narthur/summary">the Beeminder community</a>.
-                        Nathan has been <a href="https://www.beeminder.com/narthur">using Beeminder since 2018</a>. With
-                        the
-                        encouragement of Beeminder cofounder Danny and the Beeminder community, Nathan launched
-                        TaskRatchet <a href="https://blog.beeminder.com/taskratchet/">in 2020</a> to make it easier to
-                        make
-                        monetary commitments on one-off tasks. Since then he's been making <a
-                            href="https://forum.beeminder.com/t/taskratchet-development-updates/5037">steady progress</a> on
-                        turning TaskRatchet into the
-                        ultimate todo list with consequences.
-                    </p>
-                </div>
-            </div>
-            <div class="bio">
-                <div class="img">
-                    <img src="/Nicky-Walters.png" alt="Nicky Walters" />
-                </div>
-                <div class="content">
-                    <h2 class="title">Supported by Nicky</h2>
-                    <p class="details">
-                        Nicky <a href="https://breathesbooks.com/" title="Nicky's book blog!">breathes books</a> and
-                        raises
-                        rabbits. Also
-                        they are consummately <a href="https://twitter.com/shanaqui/status/1108509105650585600"
-                            title="Nicky sewing">crafty</a>, beguilingly British, and eat university degrees like candy
-                        (3
-                        and counting!).
-                        Here at TaskRatchet Nicky keeps the inbox squeaky-clean and has single-handedly improved the
-                        level
-                        of customer support by several orders of magnitude since they got involved in late 2022.
-                        Nicky's superhero name is Inbox Zero. (RIP Breakfast Bun, also pictured)
+                <div class="bio">
+                    <div class="img">
+                        <img src="/Nicky-Walters.png" alt="Nicky Walters" />
+                    </div>
+                    <div class="content">
+                        <h2 class="title">Supported by Nicky</h2>
+                        <p class="details">
+                            Nicky <a href="https://breathesbooks.com/" title="Nicky's book blog!">breathes books</a> and
+                            raises
+                            rabbits. Also
+                            they are consummately <a href="https://twitter.com/shanaqui/status/1108509105650585600"
+                                title="Nicky sewing">crafty</a>, beguilingly British, and eat university degrees like candy
+                            (3
+                            and counting!).
+                            Here at TaskRatchet Nicky keeps the inbox squeaky-clean and has single-handedly improved the
+                            level
+                            of customer support by several orders of magnitude since they got involved in late 2022.
+                            Nicky's superhero name is Inbox Zero. (RIP Breakfast Bun, also pictured)
 
-                    </p>
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div class="bio">
-                <div class="img">
-                    <img src="/Luke-Arthur.jpeg" alt="Luke Arthur" />
-                </div>
-                <div class="content">
-                    <h2 class="title">Developed by Luke</h2>
-                    <p class="details">
-                        <a href="https://github.com/lcflight">Luke Arthur</a> joined his brother to work on TaskRatchet in
-                        order to create the first version of TaskRatchet's mobile apps (coming soon!). His love of learning
-                        and engineering mindset have been a huge help in pushing TaskRatchet forward. When he's not
-                        slinging code, Luke
-                        enjoys flying helicopters, scaling climing walls, playing the piano and violin, and working on film
-                        projects.
-                    </p>
+                <div class="bio">
+                    <div class="img">
+                        <img src="/Luke-Arthur.jpeg" alt="Luke Arthur" />
+                    </div>
+                    <div class="content">
+                        <h2 class="title">Developed by Luke</h2>
+                        <p class="details">
+                            <a href="https://github.com/lcflight">Luke Arthur</a> joined his brother to work on TaskRatchet
+                            in
+                            order to create the first version of TaskRatchet's mobile apps (coming soon!). His love of
+                            learning
+                            and engineering mindset have been a huge help in pushing TaskRatchet forward. When he's not
+                            slinging code, Luke
+                            enjoys flying helicopters, scaling climing walls, playing the piano and violin, and working on
+                            film
+                            projects.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -96,12 +102,15 @@ defineProps<{
     position: relative;
     font-weight: 600;
     outline: none;
-    margin: 64px 0 0;
+    margin: 64px 0 16px;
 }
 
 .container {
     max-width: 1152px;
     margin: 16px auto;
+}
+
+.bios {
     display: flex;
     flex-wrap: wrap;
     gap: 16px;
